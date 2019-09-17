60a8f2691906870c0b84ccea562d3089cbd48c8a
Andreas Askjem <andaskjem@gmail.com>
2019-09-17 17:56:12 +0200

    Initial commit

---
+++ code.js
@@ -0,0 +1,35 @@
+// function test(){
+//     console.log(document.getElementById('menuBar'));
+//     document.getElementById('menuBar').classList.toggle('greenBackground');
+// }
+
+
+function hideElements(){
+    document.getElementById('menuOption1').classList.toggle('hide');
+    document.getElementById('menuOption2').classList.toggle('hide');
+    button = document.getElementById('menuButton');
+    button.style.background = 'pink';
+}
+
+let totalButtonPresses = 0;
+function buttonClicked(element){
+    // console.log(element.innerText)
+    totalButtonPresses += 1;
+    if (element.innerText=='Green'){
+        console.log('Green button was pressed');
+        right = document.getElementById('rightHalf');
+        right.style.background = 'green';
+    }
+    if (element.innerText=='Red'){
+        console.log('Red button was pressed');
+        right = document.getElementById('rightHalf');
+        right.style.background = 'red';
+    }
+    if (element.innerText=='Blue'){
+        console.log('Blue button was pressed');
+        right = document.getElementById('rightHalf');
+        right.style.background = 'blue';
+    }
+    let text = document.getElementById('showNumberOfPresses');
+    text.innerHTML=`You have made a total of <strong>${totalButtonPresses}</strong> button presses!`;
+}
