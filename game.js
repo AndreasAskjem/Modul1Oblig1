fd0ae3231552504eeabb48ee8177592342ca414c
AndreasAskjem <andreasa@getacademy.com>
2019-09-24 00:45:45 +0200

    fixed grid
    Grid now fills in properly, though I still want to find a good way to make the buttons square while still able to change size.
    
    Also proper randomized values are given to each button (2 of each).

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 
-changeGrid()
+//changeGrid()
 //function gridSize(){}
 
 function changeGrid(){
@@ -16,44 +16,43 @@ function changeGrid(){
     document.getElementById('theGrid').style.gridTemplateAreas = "'button1 button2 button3 button4' 'button5 button6 button7 button8' 'button9 button10 button11 button12' 'button13 button14 button15 button16' ";
 }
 
-function clickedButton(element){
+function clickedButton(element, value){
     element.style.backgroundColor = "green";
+    element.innerHTML = value;
     
 }
 
+
+//submittedNumbers()
 let buttonCSS;
 let gridTemplate;
 function submittedNumbers(){
     x = Number(document.getElementById('inputValueX').value);
     y = Number(document.getElementById('inputValueY').value);
-    console.log(x,y);
+    //x=3;
+    //y=2;
     if(validInput(x,y)){
+        let values = shuffleArray(x*y);
         let rowTemplate;
-        //let gridTemplate = '"';
-        //let buttonCSS;
-        buttonCSS = '';
-        gridTemplate = '';
+        let buttonCSS = '';
+        let gridTemplate = '';
 
         for(i=1; i<=y; i++){
             rowTemplate = `'`;
             for(j=1; j<=x; j++){
                 number = j + i*x - x;
-                console.log(number);
                 rowTemplate += `button${number} `;
                 buttonCSS += `<button type="button" id="button${number}"
                               style="grid-area: button${number};"
-                              onclick="clickedButton(this)">${number}</button>`;
+                              onclick="clickedButton(this, ${values[number-1]})">${number}</button>`;
             }
             rowTemplate += `'`;
             gridTemplate += rowTemplate;
         }
-        gridTemplate += '"';
-        console.log('aaaa');
-        document.getElementById('theGrid').style.gridTemplateAreas = gridTemplate;
         document.getElementById('theGrid').innerHTML = buttonCSS;
-        //document.getElementById('theGrid').style.gridTemplateColumns = "repeat(${x}, 1fr)";
-        //document.getElementById('theGrid').style.gridTemplateRows = "repeat(${y}, 1fr)";
-        console.log(x,y);
+        document.getElementById('theGrid').style.gridTemplateAreas = gridTemplate;
+        document.getElementById('theGrid').style.gridAutoColumns = "repeat(x, 1fr)";
+        document.getElementById('theGrid').style.gridAutoRows = "repeat(y, 1fr)";
     }
 }
 
@@ -66,28 +65,26 @@ function validInput(x,y){
     }
 }
 
-/*
-let XandYvlaues;
-function submittedNumber(){
-    let number = Number(document.getElementById('inputValue').value);
-    let XandYvlaues = mathThing(number);
-    console.log(XandYvlaues);
-    changeGrid(XandYvlaues[0], XandYvlaues[1]);
-}
 
-function mathThing(number){
-    let x = number;
-    let difference = 1000000000;
-    for(i=1; i < x+1; i++){
-        ratio = x/i;
-        if((Number.isInteger(ratio)) && (difference > Math.abs(ratio - i))){
-            difference = Math.abs(ratio - i);
-            idealX=i;
-            idealY=ratio;
-            console.log(ratio + "   " + difference + "   " + i);
-        }
+function shuffleArray(length){
+    length = length/2;
+    let arr = [];
+    for(i=0; i<length; i++){
+        arr.push(i+1);
+        arr.push(i+1);
     }
+    console.log(arr);
 
-    console.log('done');
-    return[idealX, idealY];
-}*/
+
+    let shuffledArr = [];
+    let temp;
+    for(i=arr.length; i > 0; i--){
+        
+        randomNumber = Math.floor(Math.random() * i);
+         temp = arr.splice(randomNumber,1)
+        shuffledArr.push(temp[0]);
+    
+    }
+    console.log(shuffledArr);
+    return shuffledArr;
+}
